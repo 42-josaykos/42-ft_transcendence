17db74325de5ec4545e1699d0f7968b1ae0d1aca
Jonny Saykosy <josayko@pm.me>
2022-02-16 12:12:11 +0100

    Test home page with menu

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import GamePadIcon from './icons/GamePadIcon.vue'
+import HomeItem from './HomeItem.vue'
+import HomeIcon from './icons/HomeIcon.vue'
+import BugIcon from './icons/BugIcon.vue'
+
+</script>
+
+<template>
+  <div class="wrapper">
+    <img src="../assets/42_Logo.svg" alt="42-logo" width="100" />
+    <HomeItem>
+      <template #icon>
+        <HomeIcon />
+      </template>
+      <template #heading>Home</template>
+    </HomeItem>
+
+    <HomeItem>
+      <template #icon>
+        <GamePadIcon />
+      </template>
+      <template #heading>Game</template>
+    </HomeItem>
+
+    <HomeItem>
+      <template #icon>
+        <BugIcon />
+      </template>
+      <template #heading>Debug</template>
+    </HomeItem>
+  </div>
+</template>
+
+<style>
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
---
+++ frontend/src/components/HomeItem.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="item">
+    <i>
+      <slot name="icon"></slot>
+    </i>
+    <h3 class="description">
+      <slot name="heading"></slot>
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+.item {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0.25em;
+}
+
+.description {
+  margin: 0 auto;
+  padding-left: 10px;
+}
+</style>
