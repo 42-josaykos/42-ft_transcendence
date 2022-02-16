232267c1a293ff781a5f6d53d5bfa25579266291
Jonny Saykosy <josayko@pm.me>
2022-02-16 15:37:27 +0100

    add and configure serve-static for backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path/posix';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../../frontend', 'dist'),
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
