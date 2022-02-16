61be911423db5326f789ea88e5ebeeef629c9703
Jonny Saykosy <josayko@pm.me>
2022-02-16 14:27:05 +0100

    add axios for request to backend API

--- frontend/src/components/Debug.vue
+++ frontend/src/components/Debug.vue
@@ -1,7 +1,26 @@
 <script setup lang="ts">
+import axios from 'axios'
+import { onMounted, ref } from 'vue'
+
+const data: any = ref(null)
+const url = "https://jsonplaceholder.typicode.com/todos"
+
+onMounted(() => {
+  axios.get(url).then(res =>
+    data.value = res?.data
+  ).catch(error => {
+    console.log(error);
+
+  })
+})
 
 </script>
 
 <template>
   <h2>Debug</h2>
+  <div>Tests GET requests from backend API</div>
+  <ul v-for="item in data">
+    <li>{{ item.id }}</li>
+    <li>{{ item.title }}</li>
+  </ul>
 </template>
