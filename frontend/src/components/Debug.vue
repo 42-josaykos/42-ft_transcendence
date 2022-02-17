09916c3408090d54b77c147a4c32c67b325f2411
Jonny <josayko@pm.me>
2022-02-17 16:39:43 +0100

    [Front] update tests for API

--- frontend/src/components/Debug.vue
+++ frontend/src/components/Debug.vue
@@ -3,7 +3,21 @@ import axios from 'axios'
 import { onMounted, ref } from 'vue'
 
 const data: any = ref(null)
-const url = "/api/users"
+const login: any = ref("")
+const user: any = ref({})
+
+const url = "/api/users/"
+
+// Get user by login
+async function getUserByLogin() {
+  try {
+    user.value = await axios.get(url + login.value)
+  } catch (error) {
+    console.log(error);
+    user.value = {}
+  }
+  login.value = ""
+}
 
 onMounted(() => {
   axios.get(url).then(res =>
@@ -18,6 +32,17 @@ onMounted(() => {
 <template>
   <h2>Debug</h2>
   <div>Tests GET requests to backend API</div>
+
+  <h4>Get by name</h4>
+  <form @submit.prevent.trim.lazy="getUserByLogin" method="GET">
+    <label for="name"></label>
+    <input v-model="login" name="name" type="text" />
+    <input type="submit" value="Submit" />
+  </form>
+
+  <p>{{ user.login }}</p>
+
+  <h4>Get all - id, name</h4>
   <ul v-for="item in data">
     <li>{{ item.id }}</li>
     <li>{{ item.name }}</li>
