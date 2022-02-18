e650f1552eca3cc307e60420b09b47bb4badd9e6
Ourobore <lchapren@student.42.fr>
2022-02-18 12:42:34 +0100

    Loaded config from .env file (file not pushed) with ConfigModule. Next step, .env variables validation with Joi

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path/posix';
 
@@ -10,6 +12,7 @@ import { join } from 'path/posix';
     // ServeStaticModule.forRoot({
     //   rootPath: join(__dirname, '../../frontend', 'dist'),
     // }),
+    ConfigModule.forRoot(),
     UsersModule,
   ],
   controllers: [AppController],
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,8 +5,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
 
-  const PORT = process.env.PORT || 3000;
-  await app.listen(PORT, '0.0.0.0');
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
   console.log(`Application is running on: ${await app.getUrl()}/api`);
 }
 bootstrap();
