8ae256e2820d55588c430f354f08de44ffb83df5
Ourobore <lchapren@student.42.fr>
2022-02-24 16:22:42 +0100

    Implemented routes in /users to get user stats: redirection 303 from /stats/:id. Need data validation on 'userID'

--- backend/src/api/users/users.module.ts
+++ backend/src/api/users/users.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersController } from './users.controller';
+import { StatsRedirection, UsersController } from './users.controller';
 import { StatsModule } from '../stats/stats.module';
 import UsersService from './users.service';
 import User from './entities/user.entity';
@@ -8,7 +8,7 @@ import Stats from '../stats/entities/stats.entity';
 
 @Module({
   imports: [StatsModule, TypeOrmModule.forFeature([User, Stats])],
-  controllers: [UsersController],
+  controllers: [UsersController, StatsRedirection],
   providers: [UsersService],
 })
 export class UsersModule {}
