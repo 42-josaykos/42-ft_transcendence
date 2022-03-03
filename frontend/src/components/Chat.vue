14842ed9fc3b496a1623e546e41fd14d06f7749f
Athenais DUPUY <adupuy@e2r7p19.clusters.42paris.fr>
2022-03-03 11:14:42 +0100

    functional app before websockets test

---
+++ frontend/src/components/Chat.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+ /* import { io } from 'socket.io-client';
+  import { ref } from 'vue';
+
+  const socket = io("http://localhost:3001/chat");
+
+  const message = ref<string>('')
+  //document.getElementById('message');
+  const messages = document.getElementById('messages');
+
+  const handleSubmitNewMessage = () => {
+    console.log("SUBMIT");
+    console.log("Message => ", message)
+    console.log("Message.value => ", message.value)
+    socket.emit('message', { data: message.value })
+  }
+
+  socket.on('message', ({data}) => {
+    console.log("Data = ", data)
+    handleNewMessage(data);
+  })
+
+  const handleNewMessage = (message: string) => {
+    messages.appendChild(builNewMessage(message));
+  }
+
+  const builNewMessage = (message: string) => {
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(message));
+    return li;
+  }*/
+
+</script>
+ 
+<template>
+  <h2>Chat</h2>
+ <!-- <div>
+    <ul id="messages"></ul>
+  </div>
+
+  <div>
+    <input v-model="message" type="text"/>
+    <button @click="handleSubmitNewMessage">Submit</button>
+  </div>
+-->
+</template>
