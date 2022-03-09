29cdb22e1efb597e16dbbd2f0c28dbdfc4569f0f
Ourobore <lchapren@student.42.fr>
2022-03-09 17:18:45 +0100

    Added GET routes in Message to fetch data contained in a specific message (author, channel and data for now)

--- backend/src/api/messages/messages.service.ts
+++ backend/src/api/messages/messages.service.ts
@@ -1,10 +1,11 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { channel } from 'diagnostics_channel';
 import { Repository } from 'typeorm';
 import { CreateMessageDTO } from './dto/create-message.dto';
 import { FilterMessageDTO } from './dto/filter-message.dto';
 import Message from './entities/message.entity';
+import User from 'src/api/users/entities/user.entity';
+import Channel from 'src/api/channels/entities/channel.entity';
 
 @Injectable()
 export class MessagesService {
@@ -16,17 +17,46 @@ export class MessagesService {
   async getAllMessages(): Promise<Message[]> {
     const messages = await this.messagesRepository.find({
       order: { id: 'DESC' },
+      relations: ['author', 'channel'],
     });
     return messages;
   }
 
-  async getMessageByID(messageID: number) {
+  async getMessageByID(messageID: number): Promise<Message> {
     const message = this.messagesRepository.findOne({
       where: { id: messageID },
+      relations: ['author', 'channel'],
     });
     if (!message)
       throw new NotFoundException('Message not found (id not correct)');
     return message;
+  }
+
+  async getMessageAuthor(messageID: number): Promise<User> {
+    try {
+      const message = await this.getMessageByID(messageID);
+      return message.author;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getMessageChannel(messageID: number): Promise<Channel> {
+    try {
+      const message = await this.getMessageByID(messageID);
+      return message.channel;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getMessageData(messageID: number): Promise<string> {
+    try {
+      const message = await this.getMessageByID(messageID);
+      return message.data;
+    } catch (error) {
+      throw error;
+    }
   }
 
   async getMessagesByFilter(filter: FilterMessageDTO): Promise<Message[]> {
