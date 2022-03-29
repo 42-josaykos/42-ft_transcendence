761c55de53ba9162b5c3a124fd4ab3045aa8a9f2
Ourobore <lchapren@student.42.fr>
2022-03-29 16:18:37 +0200

    Response to client with websockets is now working

--- backend/src/gateway/status.gateway.ts
+++ backend/src/gateway/status.gateway.ts
@@ -1,23 +1,28 @@
-import { Logger } from '@nestjs/common';
-import { Socket, Server } from 'socket.io';
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { Server } from 'socket.io';
+import {
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
 
 @WebSocketGateway({
   cors: {
     origin: 'http://localhost:3001',
-    // credentials: true,
+    credentials: true,
   },
 })
 export class StatusGateway {
+  @WebSocketServer()
+  server: Server;
+
   @SubscribeMessage('connection')
-  handleConection(client: any, payload: any): string {
+  handleConection(client: any, payload: any) {
     console.log('Payload: ', payload);
-    return 'Connection!';
   }
 
   @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
+  handleMessage(client: any, payload: any) {
     console.log('Payload: ', payload);
-    return 'Hello world!';
+    this.server.sockets.emit('message', 'Hello World!');
   }
 }
