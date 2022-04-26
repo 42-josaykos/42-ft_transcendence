1b04f88778265e81bb3791d6eebf08e45f3ff02e
Ourobore <lchapren@student.42.fr>
2022-04-26 15:02:46 +0200

    Synchronized players start of the game: players can queue with a button, and when there is 2 players, the match start

--- backend/src/game/game.gateway.ts
+++ backend/src/game/game.gateway.ts
@@ -15,7 +15,7 @@ import { Server, Socket } from 'socket.io';
 import User from 'src/api/users/entities/user.entity';
 
 class Connection {
-  userID: number;
+  user: User;
   socketID: string[];
 }
 
@@ -39,9 +39,11 @@ export class GameGateway
 
   private logger: Logger = new Logger('GameGateway');
   private games: Game[];
+  private queue: Connection[];
 
   afterInit(server: any) {
     this.games = new Array();
+    this.queue = new Array();
     this.logger.log('Game gateway is initialized');
   }
 
@@ -53,11 +55,30 @@ export class GameGateway
     this.logger.log(`Disconnect: ${client.id}`);
   }
 
+  @SubscribeMessage('queue')
+  handleQueue(@ConnectedSocket() client, @MessageBody() data: User) {
+    // Adding player to queue
+    this.queue.push({ user: data, socketID: [client.id] });
+
+    // Start a game if there is at least 2 players in the queue waiting
+    while (this.queue.length >= 2) {
+      // Remove players from queue
+      const playerOne = this.queue.shift();
+      const playerTwo = this.queue.shift();
+
+      // Start game
+      this.server
+        .to(playerOne.socketID[0])
+        .to(playerTwo.socketID[0])
+        .emit('startGame', { playerOne: playerOne, playerTwo: playerTwo });
+    }
+  }
+
   @SubscribeMessage('moveLeft')
   handleMoveLeft(@ConnectedSocket() client: Socket, @MessageBody() data: User) {
-    const gameIndex = this.games.findIndex((game) =>
-      game.players.findIndex((connection) => connection.userID === data.id),
-    );
+    // const gameIndex = this.games.findIndex((game) =>
+    //   game.players.findIndex((connection) => connection.userID === data.id),
+    // );
 
     // Should never append, but prevention is better than cure
     // if (gameIndex === -1) {
@@ -78,9 +99,9 @@ export class GameGateway
     @ConnectedSocket() client: Socket,
     @MessageBody() data: User,
   ) {
-    const gameIndex = this.games.findIndex((game) =>
-      game.players.findIndex((connection) => connection.userID === data.id),
-    );
+    // const gameIndex = this.games.findIndex((game) =>
+    //   game.players.findIndex((connection) => connection.userID === data.id),
+    // );
 
     // Should never append, but prevention is better than cure
     // if (gameIndex === -1) {
