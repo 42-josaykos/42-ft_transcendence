e6b1931b53d7505032993189d02d1fe8a7a4802e
Athenais DUPUY <adupuy@e2r5p22.clusters.42paris.fr>
2022-05-03 13:28:17 +0200

    add guard when logout

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -191,6 +191,7 @@ export class AuthController {
    */
   @Get('logout')
   @Redirect('/')
+  @UseGuards(JwtTwoFactorGuard)
   async logout(@Req() req, @Res() res: Response) {
     await this.authService.removeRefreshToken(req.user.id);
     res.setHeader('Set-Cookie', this.authService.getCookieForLogout());
